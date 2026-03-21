945b2691ed7b23e4e604de9d08dacfda2db9ba58
vishwas <t8302419@gmail.com>
2026-03-21 03:28:25 -0700

    updated10

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -2,15 +2,20 @@
 import axios from "axios";
 import react, { useState } from "react"
 const Show= ()=>{
-     const[product,setProduct]=useState([])
+     const[product,setProduct]=useState([]);
+     const[loading,setLoadoading]= useState(false);
+
     // setProduct(product.data);
     const getProduct =()=>{
         const api='https://fakestoreapi.com/products';
-        axios.get(api).then(products=>{ 
-             console.log(products);
-             setProduct(products.data)
+        setLoadoading(true);
+        axios.get(api).then(res=>{ 
+             console.log(res.data);
+             setProduct(res.data);
             
-            } ).catch(err=>{console.log(err)});
+            } ).catch(err=>{console.log(err)}).finally(()=>{
+                setLoadoading(false);
+            });
  
     };
  const addProduct =()=>{
@@ -31,9 +36,10 @@ const Show= ()=>{
             <br />
             <button onClick={addProduct} className="px-5 py-2 bg-red-300 text-zinc-300 rounded mt-10 hover:bg-red-600">save new product</button>
             <div className="mt-10">
-                <ul>{product.length>0 ? ( product.map((p)=>(<li key={p.id} className="p-3 bg-red-300 rounded mb-2 w-1/4">{p.title}</li>))):(<h1>loading...</h1>)}</ul>
-            </div>
+                {loading ?( <h1>...loading</h1>):product.length===0 ?(<h1>no product found</h1>):<ul>
+                    (product.map((p)=>{<li key={p.id} className="p-3 bg-red-300 rounded mb-2 w-1/4">{p.title}</li>}))}  </ul>
           
+        </div>
         </div>
 
     )
